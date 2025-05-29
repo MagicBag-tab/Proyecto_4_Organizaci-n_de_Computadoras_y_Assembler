47e03dc39c2005df0b16ff185d8ad23a21279269
Sarah <157857933+MagicBag-tab@users.noreply.github.com>
2025-05-28 19:16:21 -0600

    Create main.s con encabezado para el proyecto 4 en assembly

---
+++ main.s
@@ -0,0 +1,13 @@
+/*
+**********************************************************************************************
+*@file      main.s
+*@author    Sarah Estrada
+*			      Kristel Castillo
+*			      Yu-Fong Chen
+*@date      May, 2025 - June 2025
+*@brief     Proyecto 4, Programa contador de 0 a 9 en Assembly al que se le puede
+*           aumentar o disminuir la velocidad.
+*@details
+*			      - 
+**********************************************************************************************
+*/
